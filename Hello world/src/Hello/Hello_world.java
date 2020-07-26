ca5b55fc846906db5af8847da968a64d435503f2
Jay <junyeongpark96@gmail.com>
2020-07-26 15:58:35 +0900

    practice 1 for git

--- Hello world/src/Hello/Hello_world.java
+++ Hello world/src/Hello/Hello_world.java
@@ -3,6 +3,7 @@ package Hello;
 public class Hello_world {
 	public static void main(String[] args) {
         System.out.println("Hello world");
+       System.out.println("Hello worold2");
     }
 
 }
